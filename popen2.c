477d6977926962d6c2156b796fd6825cf44fd3f0
yurenchen <yurenchen@yeah.net>
2019-02-13 06:52:49 +0800

    U popen2.c: fix popen2() close pipe fd

--- popen2.c
+++ popen2.c
@@ -30,13 +30,17 @@ int popen2(const char *cmdline, struct popen2 *childinfo) {
     p = fork();
     if(p < 0) return p; /* Fork failed */
     if(p == 0) { /* child */
-        close(pipe_stdin[1]);
+		// child close pipe dumb fd
+        close(pipe_stdin[1]); //parent w
+        close(pipe_stdout[0]);//parent r
         dup2(pipe_stdin[0], 0);
-        close(pipe_stdout[0]);
         dup2(pipe_stdout[1], 1);
         execl("/bin/sh", "sh", "-c", cmdline, (void*)0);
         perror("execl"); exit(99);
     }
+	// parent close pipe dumb fd
+	close(pipe_stdin[0]); //child r
+	close(pipe_stdout[1]);//child w
     childinfo->child_pid = p;
     childinfo->to_child = pipe_stdin[1];
     childinfo->from_child = pipe_stdout[0];
@@ -51,12 +55,18 @@ int main(void) {
     popen2("tr a-z A-Z", &kid);
     write(kid.to_child, "testing\n", 8);
     close(kid.to_child);
-    memset(buf, 0, 1000);
-    read(kid.from_child, buf, 1000);
-    printf("kill(%d, 0) -> %d\n", kid.child_pid, kill(kid.child_pid, 0)); 
-    printf("from child: %s", buf); 
+
+	printf("kill(%d, 0) -> %d\n", kid.child_pid, kill(kid.child_pid, 0));  //check if exit
+
+	int n = 0;
+	do {
+		memset(buf, 0, 1000);
+		n = read(kid.from_child, buf, 1000);
+		printf("from child (%d): %s", n, buf); 
+	}while(n>0);
+
     printf("waitpid() -> %d\n", waitpid(kid.child_pid, NULL, 0));
-    printf("kill(%d, 0) -> %d\n", kid.child_pid, kill(kid.child_pid, 0)); 
+	printf("kill(%d, 0) -> %d\n", kid.child_pid, kill(kid.child_pid, 0));  //check if exit
     return 0;
 }
 #endif
